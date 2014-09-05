cd4b8d4ccf9378d2c256bb9b37c507311a1a4174
Valentino Wong <valentino.wong@gmail.com>
2014-09-04 23:37:53 -0400

    Add names to users.

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,8 @@ class Pin < ActiveRecord::Base
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment :image, content_type: { content_type: /\Aimage\/.*\Z/ }
+
+	validates :image, presence: true
+	validates :description, presence: true
+
 end
